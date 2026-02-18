4a06553dc54aa604d9f3d34cd9fa69f3a3a85716
Clement Dieperink <clement.dieperink@heig-vd.ch>
2026-02-18 08:40:22 +0100

    add labo1

---
+++ labo1/hps_gpio/soft/src/hps_gpio.c
@@ -0,0 +1,33 @@
+/*****************************************************************************************
+ * HEIG-VD
+ * Haute Ecole d'Ingenerie et de Gestion du Canton de Vaud
+ * School of Business and Engineering in Canton de Vaud
+ *****************************************************************************************
+ * REDS Institute
+ * Reconfigurable Embedded Digital Systems
+ *****************************************************************************************
+ *
+ * File                 : hps_gpio.c
+ * Author               : 
+ * Date                 : 
+ *
+ * Context              : SCF tutorial lab
+ *
+ *****************************************************************************************
+ * Brief: light HPS user LED up when HPS user button pressed, for DE1-SoC board
+ *
+ *****************************************************************************************
+ * Modifications :
+ * Ver    Date        Student      Comments
+ * 
+ *
+*****************************************************************************************/
+#define .....
+
+int main(void){
+    
+    /*
+    * TO DO
+    */
+
+}
