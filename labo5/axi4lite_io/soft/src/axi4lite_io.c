10d7733e36a2dfc0aa6e7da622d6a46ea8bf7cea
Clement Dieperink <clement.dieperink@heig-vd.ch>
2026-04-01 14:38:34 +0200

    add labo5

---
+++ labo5/axi4lite_io/soft/src/axi4lite_io.c
@@ -0,0 +1,33 @@
+/*****************************************************************************************
+ * HEIG-VD
+ * Haute Ecole d'Ingenerie et de Gestion du Canton de Vaud
+ * School of Business and Engineering in Canton de Vaud
+ *****************************************************************************************
+ * REDS Institute
+ * Reconfigurable Embedded Digital Systems
+ *****************************************************************************************
+ *
+ * File                 : axi4lite_io.c
+ * Author               :
+ * Date                 :
+ *
+ * Context              : SCF tutorial lab
+ *
+ *****************************************************************************************
+ * Brief: Test application for AXI4-Lite FPGA IO laboratory
+ *
+ *****************************************************************************************
+ * Modifications :
+ * Ver    Date        Student      Comments
+ *
+ *
+*****************************************************************************************/
+#define .....
+
+int main(void){
+
+    /*
+    * TO DO
+    */
+
+}
